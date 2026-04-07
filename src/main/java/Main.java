c8be64c8515ec43bb18e86f8356f61b4de9492e8
Maira <mairahumaira7@hotmail.co.uk>
2026-04-07 13:43:06 +0000

    part2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args){
+        Greeter greeter = new Greeter();
+        Calculator calculator = new Calculator();
+
+        greeter.greet("World");
+        System.out.println(calculator.add(3, 5));
+    }
 }
